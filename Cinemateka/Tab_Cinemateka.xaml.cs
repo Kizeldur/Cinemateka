51586d702a45aded7078c2bbb14ecb6b7ab2308f
Student RPO <Student RPO@DESKTOP-QMVA7M7>
2021-09-22 21:48:15 +0300

    Разнес xaml

---
+++ Cinemateka/Tab_Cinemateka.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KinopoiskAPI;
+
+namespace Cinemateka
+{
+    /// <summary>
+    /// Логика взаимодействия для Tab_Cinemateka.xaml
+    /// </summary>
+    public partial class Tab_Cinemateka : UserControl
+    {
+        
+        public Tab_Cinemateka()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void Button_SearchDB_Click(object sender, RoutedEventArgs e)
+        {
+            var movie = new Movie { Id = "798", Title = "qwe" };
+            var m = new Tab_Movie();
+            m.ShowMovie(movie);
+        }
+
+        private void DataTable_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var tab_movie = new Tab_Movie();
+            var item = (CinematekaTable)DataTable.SelectedItem as CinematekaTable;
+            var title = item.Title;
+            var movie = KinopoiskApi.GetMovieByTheTitle(title);
+            tab_movie.ShowMovie(movie);
+            //tab_movie.tabcontrol_Cinemateka.SelectedItem = tab_movie.tab_Movie;
+            e.Handled = true;
+        }
+    }
+}
---
+++ Cinemateka/Tab_Movie.xaml.cs
@@ -0,0 +1,46 @@
+﻿using KinopoiskAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cinemateka
+{
+    /// <summary>
+    /// Логика взаимодействия для Tab_Movie.xaml
+    /// </summary>
+    public partial class Tab_Movie : UserControl
+    {
+        public Tab_Movie()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void ShowMovie(Movie movie)
+        {
+            //var path = "https:" + movie.Poster;
+            //var image = new BitmapImage(new Uri(path, UriKind.Absolute));
+            //image_Poster.Source = image;
+            label_Title.Content = movie.Title;
+            //label_OriginalTitle.Content = movie.Title_Alternative;
+            //label_Director.Content = movie.Directors[0];
+            //label_Actors.Content = $"{movie.Actors[0]}, {movie.Actors[1]}, {movie.Actors[2]}";
+            //label_Description.Content = movie.Description;
+            //label_KinopoiskRating.Content = movie.Rating_Kinopoisk;
+            //label_IMDbRating.Content = movie.Rating_Kinopoisk;
+
+        }
+    }
+}
